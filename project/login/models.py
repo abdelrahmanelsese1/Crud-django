1b1933ff94c8ca6696c1a34b8005aa6ba0a42f2e
Abdelrahman <abdelrahmanmohamedkhaleel@gmail.com>
2022-02-01 02:13:12 +0200

    register

--- project/login/models.py
+++ project/login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class SignupInfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+
+
+
+
--- project/login/views.py
+++ project/login/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from .models import SignupInfo
 
 # Create your views here.
 def login(request):
-    context={}
+    context = {}
     return render(request, 'login/login.html', context)
+
+def signup(request):
+    context = {}
+    context['ID'] = 1
+    if (request.method == 'GET'):
+        return render(request, 'login/signup.html', context)
+    else:
+
+        # intsake reques.post
+        print(request.POST)
+        name = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        SignupInfo.objects.create(name=name, email=email, password=password)
+        signupinfos = SignupInfo.objects.all()
+        context['SignupInfo'] = signupinfos
+        context['msg'] = 'user inserted'
+        return render(request, 'login/login.html', context)
+
